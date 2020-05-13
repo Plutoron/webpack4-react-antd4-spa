c9b8988038c846b85efdb9eb13e24853c5223e30
suyunlongsy <suyunlongsy@gmail.com>
2020-05-13 14:19:52 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -65,7 +65,7 @@ module.exports = (env, argv) => {
       },
       minimizer: [
         ...(isDEV ? [] : [
-          new TenserPlugin()
+          new TerserPlugin()
 				]),
         new OptimizeCSSAssetsPlugin({
           assetNameRegExp: /\.css$/g,
